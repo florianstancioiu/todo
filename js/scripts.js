15b38fb7fca4c804af1e170fa5035afc61b2fca9
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2024-01-28 17:39:13 +0200

    Added youtube video code

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,34 @@
+'use strict';
+
 const input = document.querySelector('.todo-input');
 const addTodoBtn = document.querySelector('.todo-add-btn');
 const todoBody = document.querySelector('.todo-body');
 const template = document.querySelector('#todo-item-template');
+
+const renderTodosHTML = () => {
+  if (!localStorage.getItem('todos')) {
+    return;
+  }
+
+  // clear the todo body
+  todoBody.innerHTML = '';
+
+  const todos = JSON.parse(localStorage.getItem('todos'));
+
+  todos.forEach((todo, index) => {
+    const templateClone = template.content.cloneNode(true);
+    const todoItem = templateClone.querySelector('.todo-item');
+    const title = templateClone.querySelector('.todo-title');
+    title.textContent = todo.title;
+    todoItem.setAttribute('data-index', index);
+
+    if (todo.isCompleted) {
+      todoItem.classList.add('todo-completed');
+    }
+
+    todoBody.appendChild(templateClone);
+  });
+};
 
 // Task 1
 const addTodo = () => {
@@ -11,16 +38,35 @@ const addTodo = () => {
   }
 
   // Task 6
-  const editableTodoItem = document.querySelector('.todo-item.active');
+  const editableTodoItem = document.querySelector('.todo-item.todo-editable');
   if (editableTodoItem) {
-    const title = editableTodoItem.querySelector('.todo-title');
-    title.textContent = input.value;
+    const editableTodoIndex = parseInt(
+      editableTodoItem.getAttribute('data-index')
+    );
+
+    // retrive the data from localstorage
+    const todos = JSON.parse(localStorage.getItem('todos'));
+
+    // create a new array using map with the right data
+    const newTodos = todos.map((todo, index) => {
+      if (index === editableTodoIndex) {
+        return {
+          ...todo,
+          title: input.value,
+        };
+      }
+
+      return todo;
+    });
+
+    // set the data back in localstorage
+    localStorage.setItem('todos', JSON.stringify(newTodos));
+
+    // render the html
+    renderTodosHTML();
 
     // clear the input
     input.value = '';
-
-    // remove active class from todo item
-    editableTodoItem.classList.remove('active');
 
     // reset the add button name
     addTodoBtn.textContent = 'Add Item';
@@ -36,14 +82,23 @@ const addTodo = () => {
     return;
   }
 
-  // create a clone of the template content
-  // fill it with the title of the todo
-  // append it inside the container
-  const inputValue = input.value;
-  const templateClone = template.content.cloneNode(true);
-  const templateCloneTitle = templateClone.querySelector('.todo-title');
-  templateCloneTitle.textContent = inputValue;
-  todoBody.appendChild(templateClone);
+  let todos = [];
+  if (localStorage.getItem('todos')) {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+
+  const newTodo = {
+    title: input.value,
+    isCompleted: false,
+  };
+
+  todos.push(newTodo);
+
+  const stringifiedTodos = JSON.stringify(todos);
+
+  localStorage.setItem('todos', stringifiedTodos);
+
+  renderTodosHTML();
 
   // clear the input value
   input.value = '';
@@ -64,7 +119,26 @@ const deleteTodo = (event) => {
     return;
   }
 
-  currentTarget.closest('.todo-item').remove();
+  const currentTodo = currentTarget.closest('.todo-item');
+  const currentTodoIndex = parseInt(currentTodo.getAttribute('data-index'));
+
+  const todos = JSON.parse(localStorage.getItem('todos'));
+
+  const newTodos = todos.filter((todo, index) => {
+    // make sure that the currently clicked todo is not part of newTodos
+    if (index === currentTodoIndex) {
+      return false;
+    }
+
+    // make sure that the rest of the elements are inside the array
+    return true;
+  });
+
+  // set the localstorage data
+  localStorage.setItem('todos', JSON.stringify(newTodos));
+
+  // render the html
+  renderTodosHTML();
 };
 
 // Task 4
@@ -75,7 +149,29 @@ const toggleTodo = (event) => {
     return;
   }
 
-  currentTarget.closest('.todo-item').classList.toggle('completed');
+  const currentTodo = currentTarget.closest('.todo-item');
+  const currentTodoIndex = parseInt(currentTodo.getAttribute('data-index'));
+
+  // retrieve the data from localstorage
+  const todos = JSON.parse(localStorage.getItem('todos'));
+
+  // create a new array with the right data
+  const newTodos = todos.map((todo, index) => {
+    if (index === currentTodoIndex) {
+      return {
+        ...todo,
+        isCompleted: !todo.isCompleted,
+      };
+    }
+
+    return todo;
+  });
+
+  // set the new data to localstorage
+  localStorage.setItem('todos', JSON.stringify(newTodos));
+
+  // render the html
+  renderTodosHTML();
 };
 
 // Task 5
@@ -92,10 +188,10 @@ const editTodo = (event) => {
   const todoItems = document.querySelectorAll('.todo-item');
 
   todoItems.forEach((todo) => {
-    todo.classList.remove('active');
+    todo.classList.remove('todo-editable');
   });
 
-  todoItem.classList.add('active');
+  todoItem.classList.add('todo-editable');
 
   input.value = title.textContent;
 
@@ -121,15 +217,28 @@ const dropTodo = (event) => {
   const droppedOnTodo = currentTarget.closest('.todo-item');
   const activeDraggedTodo = document.querySelector('.todo-item.active-drag');
 
-  const indexOfDropped = Array.from(todoBody.childNodes).indexOf(droppedOnTodo);
-  const indexOfActive = Array.from(todoBody.childNodes).indexOf(
-    activeDraggedTodo
-  );
+  const droppedIndex = parseInt(droppedOnTodo.getAttribute('data-index'));
+  const activeIndex = parseInt(activeDraggedTodo.getAttribute('data-index'));
 
-  const reference =
-    indexOfActive < indexOfDropped ? droppedOnTodo.nextSibling : droppedOnTodo;
+  // retrieve the localstorage data
+  const todos = JSON.parse(localStorage.getItem('todos'));
 
-  todoBody.insertBefore(activeDraggedTodo, reference);
+  // retrive the active todo object from todos array
+  const activeTodo = todos[activeIndex];
+
+  // update the existing todos array using splice
+
+  // remove the active todo object from todos array
+  todos.splice(activeIndex, 1);
+
+  // push the active todo object in the todos array at the right location
+  todos.splice(droppedIndex, 0, activeTodo);
+
+  // set the localstorage data back in
+  localStorage.setItem('todos', JSON.stringify(todos));
+
+  // render the html
+  renderTodosHTML();
 
   activeDraggedTodo.classList.remove('active-drag');
 };
@@ -143,3 +252,5 @@ todoBody.addEventListener('click', editTodo);
 todoBody.addEventListener('dragstart', dragStartTodo);
 todoBody.addEventListener('dragover', dragOverTodoBody);
 todoBody.addEventListener('drop', dropTodo);
+
+renderTodosHTML();
